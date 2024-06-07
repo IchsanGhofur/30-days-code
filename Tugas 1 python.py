ff1eefa8f03b9f5cf3d3d9eca05087245c2fd9e3
IchsanGhofur <a710230011@student.ums.ac.id>
2024-06-07 23:32:34 +0700

    Add files via upload

---
+++ Tugas 1 python.py
@@ -0,0 +1,14 @@
+print("Data diri")
+#mengambil inputan nama
+nama = input('Siapa nama kamu : ')
+
+#mengambil inputan nim
+nim = input ('Masukkan nilai nim: ')
+
+#mengambil inputan angkatan
+angkatan = input ('Masukkan nilai angkatan: ')
+
+#menampilkan hasil
+print('nama', nama)
+print('nim', nim)
+print('angkatan', angkatan)
